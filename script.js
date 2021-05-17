812085f9a5c57d9de5d5ad479be23a0996f940ff
Mihail-Balamatiuc <mihail001.balamatiuc@mail.ru>
2021-05-17 22:55:24 +0300

    edit1

--- script.js
+++ script.js
@@ -1,22 +1,39 @@
+/*"use strict";
+
+var n = 1;
+
+function change1 ()
+{	if ( n == 1){
+		let a = document.querySelector('link[href="style.css"]');
+		a.href = "night.css";
+		document.getElementById("dlmode").src="sun.jpg";
+		n = 0;}
+	else{
+		let a = document.querySelector('link[href="night.css"]');
+		a.href = "style.css";
+		document.getElementById("dlmode").src="moon.jpg";
+		n = 1;}
+}*/
+
 "use strict";
 
 var n = 1;
 
 function change1 ()
 {	if ( n == 1){
-		document.getElementById("bd").style.backgroundImage="url(dark.jpg)";
+		document.getElementById("bd").style.backgroundImage="url(dark2.jpg)";
 		document.getElementById("dlmode").src="sun.jpg";
 		document.getElementById("text1").style.color="white";
 		document.getElementById("text2").style.color="white";
 		document.getElementById("navbar").style.borderBottom = "1px solid white";
-		document.getElementById("navbar").style.backgroundImage="url(dark.jpg)";
+		document.getElementById("navbar").style.backgroundImage="url(dark2.jpg)";
 		n = 0;}
 	else{
-		document.getElementById("bd").style.backgroundImage="url(light.jpg)";
+		document.getElementById("bd").style.backgroundImage="url(light2.jpg)";
 		document.getElementById("dlmode").src="moon.jpg";
 		document.getElementById("text1").style.color="black";
 		document.getElementById("text2").style.color="black";
 		document.getElementById("navbar").style.borderBottom = "1px solid black";
-		document.getElementById("navbar").style.backgroundImage="url(light.jpg)";
+		document.getElementById("navbar").style.backgroundImage="url(light2.jpg)";
 		n = 1;}
 }
